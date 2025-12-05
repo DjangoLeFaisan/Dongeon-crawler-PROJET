05fcadef140d4ca5c25344ecdfe383438cfc75e2
BobyPogba <gabriel.boby@outlook.fr>
2025-12-05 16:12:00 +0100

    le code au caca de simon

--- src/battle.c
+++ src/battle.c
@@ -1,57 +1,38 @@
 /*systeme de combat en tour par tour , tes sorts ou tes actions possibles sont visibles tout le temps 
 une fois qu'un enemie t'as vu le tour par tour commence chacun attaque et en haut une barre de priorité s'affiche pile ou face pour savoir qui commence, 
-possibilité si l'enemie ne te voit pas d'attaquer en premier il va alors prendre les dégats puis lancer le combat 
+possibilité si l'enemie ne te voit pas d'attaquer en premier il va alors prendre les dégats puis lancer le combat*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "raylib.h"
+#include "game.h"
 
 
 
+int test( int resultatordi ) {
+
+    // Active tous les logs
+    SetTraceLogLevel(LOG_ALL);
+
+    // Affichage des options 
 
 
-int test() {
-int choixJoueurs, resultatordi; 
-fonctions pour stocker le choix du joueurs et le resultat*/
-/* int lancedés(); {
- 
-     
-       srand((unsigned)time(NULL)); // Initialisation de la génération de nombres aléatoires en fonction du temps actuel
-    resultatordi = rand() % 2; // Génère un nombre aléatoire entre 0 et 1 pour simuler le côté de la pièce
+    // Initialisation du générateur aléatoire
+    int GetRandomValue(int min  , int max );
+     resultatordi = rand() % 2;
 
-    // Affichage des options pour le joueur
-    printf("Choisissez un côté :\n");
-    printf("0. Pile\n");
-    printf("1. Face\n");
-    scanf("%d", &choixJoueurs); 
+    // Vérification des résultats + enlever le print remplacer par le log 
+    if (resultatordi == 1) {
+        TraceLog(LOG_INFO,"Le monstre commence à attaquer !");
+                           }
+    else {
+        TraceLog(LOG_INFO,"Le joueur commence à attaquer !");
 
-    // Vérification des résultats
-    if (choixJoueurs == resultatordi) { 
-        if (resultatordi == 0) { 
-            printf("La pièce est tombée sur Pile. Vous avez gagné !\n");
-        } else { 
-            printf("La pièce est tombée sur Face. Vous avez gagné !\n");
+         } 
+
+
         }
-    } else { 
-        if (resultatordi == 0) { 
-            printf("La pièce est tombée sur Pile. Vous avez perdu.\n");
-        } else { // 
-            printf("La pièce est tombée sur Face. Vous avez perdu.\n");
-        }
-    }
-
-    return 0; 
-} ;
-
- if (resultatordi==choixJoueurs ) {
-     printf("le joueur commence à attaquer ! \n" );
-   }
-   else {
-     printf("le monstre commence commence à attaquer ! " );
-   };
-   return 0;
-  
-}
-*/
 
 
 
