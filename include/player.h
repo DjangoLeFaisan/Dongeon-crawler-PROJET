c2823c6f0b7a0309cc5fc40215b34240c6ee8853
Django ALIZON <djangoetarko@gmail.com>
2025-12-08 11:54:33 +0100

    Fix ombre

--- include/player.h
+++ include/player.h
@@ -3,8 +3,6 @@
 typedef struct Player {
     int gridX;
     int gridY;
-    float pixelX;       // Position en pixels pour l'animation
-    float pixelY;       // Position en pixels pour l'animation
     int speed;
     int texture_id;
     Texture2D texture;
