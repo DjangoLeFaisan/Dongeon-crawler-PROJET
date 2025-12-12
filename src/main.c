93f4a814aa0909d71fbae75019ef422fb5b502d5
Jules1937 <jules.dixneuf0@gmail.com>
2025-12-12 12:20:53 +0100

    telugouzgofbfo

--- src/main.c
+++ src/main.c
@@ -90,20 +90,10 @@ int main(void)
     Board board = {0};
     GameInit(&board);
 
-<<<<<<< HEAD
     // Initialiser le système de combat
     extern CombatState gCombatState;
     InitCombat(&gCombatState);
     
-=======
-    if (MapLoad(&board, "maps/couloir_defaul.map")) {
-        TraceLog(LOG_INFO, "Carte chargée avec succès");
-    } else {
-        TraceLog(LOG_ERROR, "Erreur lors du chargement de la carte");
-    }
-    extern bool special_level;
-    special_level = true;
->>>>>>> 10de506f45d89035d5d9366e252d08594b5089bb
         
     while (!WindowShouldClose())
     {
